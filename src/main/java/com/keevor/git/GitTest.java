45a25e4774f08600b6c8ef94c2f875f81fccbf7e
keevor <976241501@qq.com>
2022-03-10 12:52:59 +0800

    second commit

--- src/main/java/com/keevor/git/GitTest.java
+++ src/main/java/com/keevor/git/GitTest.java
@@ -3,5 +3,6 @@ package com.keevor.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git！");
+        System.out.println("Hello Git2!");
     }
 }
