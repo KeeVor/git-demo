b86f5be3d32c24a987c8a18c756285e3822534c3
keevor <976241501@qq.com>
2022-03-10 13:04:18 +0800

    hot-fix

--- src/main/java/com/keevor/git/GitTest.java
+++ src/main/java/com/keevor/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello Git！");
         System.out.println("Hello Git2!");
         System.out.println("Hello Git3!");
+        System.out.println("hot-fix");
     }
 }
